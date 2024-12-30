cc8a05579be83249a41cc37c1f28a7a189c7ac79
paull <paullopisparrilla@gmail.com>
2024-12-30 15:28:05 +0000

    Model package completed.

--- src/main/java/software/ulpgc/model/Title.java
+++ src/main/java/software/ulpgc/model/Title.java
@@ -1,4 +1,18 @@
 package software.ulpgc.model;
 
-public class Title {
+public record Title(String id, TitleType titleType, String primaryTitle) {
+
+    public enum TitleType{
+        VideoGame,
+        TvPilot,
+        Movie,
+        tvSeries,
+        TvMiniSeries,
+        Short,
+        TvSpecial,
+        TvShort,
+        Video,
+        TvMovie,
+        TvEpisode
+    }
 }
